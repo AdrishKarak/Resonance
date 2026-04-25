c6a5636be3e9a0ca70395877ea777e19e1c543ba
Adrish <runakarak@gmail.com>
2026-04-26 00:58:33 +0530

    feat: implement comprehensive UI component library based on Shadcn UI and add core application pages

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/nextjs";
+
+
+export default function SignInPage() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <SignIn appearance={{
+                elements: {
+                    rootBox: "mx-quto",
+                    card: "shadow-lg"
+                }
+            }} />
+        </div>
+    )
+}
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignUp } from "@clerk/nextjs";
+
+
+export default function SignUpPage() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <SignUp appearance={{
+                elements: {
+                    rootBox: "mx-quto",
+                    card: "shadow-lg"
+                }
+            }} />
+        </div>
+    )
+}
